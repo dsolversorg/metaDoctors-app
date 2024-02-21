b8c0b63b4a04deada880e23bf28fc4795061ab1c
Deison Braga <deisonbraga@hotmail.com>
2024-02-21 03:07:46 -0300

    primeiro teste

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 export const applyCustomCode = (externalCodeSetup: any) => {
-	alert('Soooo dalee');
+	// alert('Soooo dalee');
+	externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text>This is my signup screen.</Text>
+        </View>
+    ));
 };
