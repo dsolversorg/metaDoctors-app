cf634ebacb5031e1322f25482b1597c26690c702
Deison Braga <deisonbraga@hotmail.com>
2024-02-21 03:15:30 -0300

    primeiro teste

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 export const applyCustomCode = (externalCodeSetup: any) => {
-	// alert('Soooo dalee');
-	externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
-        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Text>This is my signup screen.</Text>
-        </View>
-    ));
+	// call custom code api here
+	alert('teste');
 };
