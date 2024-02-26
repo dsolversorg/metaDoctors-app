dfae2712a3d815a863b6ab29ee64a9bd98a1ca1a
QuatrochiFh <quatrochifabio145@gmail.com>
2024-02-26 15:25:43 -0300

    tela de cadastro iniciada

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-export const applyCustomCode = (externalCodeSetup: any) => {
-	// call custom code api here    
-	};
+import React from 'react';
+import { StatusBar, View, Text } from 'react-native';
+export const applyCustomCode = externalCodeSetup => {
+	externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text>Tela de cadastro vamo tentar ver essa bagaca</Text>
+			<TextInput placeholder={"login"} />
+			<TextInput placeholder={"password"} />
+		</View>
+	));
+}
